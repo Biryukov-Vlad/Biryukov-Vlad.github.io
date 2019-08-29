62e1f4e945d8cc1e291ce71f6d3fa6c25eed1d2f
Vlad <ksand.1@ya.ru>
2019-08-29 13:07:29 +0400

    собрал проект, подправил 8 дз

---
+++ BASKET.js
@@ -0,0 +1,24 @@
+// var dateSlider = document.getElementsByClassName("slider__date_js")[0]
+// var date = new Date()
+// var input_Y = document.getElementsByClassName("slider__input_js")[0]
+// var input_M = document.getElementsByClassName("slider__input_js")[1]
+// var input_D = document.getElementsByClassName("slider__input_js")[2]
+// var button = document.getElementsByClassName("slider__button_js")[0]
+// var result = document.getElementsByClassName("slider__result_js")[0]
+// var days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+// var body = document.body;
+
+// body.addEventListener("keydown", (e) => {
+//     console.log(e.keyCode);
+
+// })
+// console.log(date);
+// dateSlider.innerHTML = date;
+// button.addEventListener("click", function name() {
+//     dateSlider.innerHTML = new Date(input_Y.value, input_M.value, input_D.value);
+//     var ret = new Date()
+//     result.innerHTML = days[ret.getDay()]
+// })
+
+
+ 
