25f23e9e8cf8e7ce5c18be0fc0f616602267825f
Vlad <ksand.1@ya.ru>
2019-09-02 17:05:18 +0400

    dz8 dz9

--- BASKET.js
+++ BASKET.js
@@ -1,24 +1,100 @@
-// var dateSlider = document.getElementsByClassName("slider__date_js")[0]
-// var date = new Date()
-// var input_Y = document.getElementsByClassName("slider__input_js")[0]
-// var input_M = document.getElementsByClassName("slider__input_js")[1]
-// var input_D = document.getElementsByClassName("slider__input_js")[2]
-// var button = document.getElementsByClassName("slider__button_js")[0]
-// var result = document.getElementsByClassName("slider__result_js")[0]
-// var days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
-// var body = document.body;
+// var button = document.getElementsByClassName("button_js")[0]
+// var button1 = document.getElementsByClassName("button_js")[1]
+// var container = document.getElementsByClassName("container_js")[0]
+// var input = document.getElementsByClassName("input-text_js")[0]
+// var input2 = document.getElementsByClassName("input-text_js")[1]
 
-// body.addEventListener("keydown", (e) => {
-//     console.log(e.keyCode);
+// var arr = []
 
-// })
-// console.log(date);
-// dateSlider.innerHTML = date;
-// button.addEventListener("click", function name() {
-//     dateSlider.innerHTML = new Date(input_Y.value, input_M.value, input_D.value);
-//     var ret = new Date()
-//     result.innerHTML = days[ret.getDay()]
+
+// button.addEventListener("click", (event) => {
+//     // input.value;  
+    // var reg = new RegExp(".\s\w", "gi")
+    // console.log(`${input.value}`.replace(/`${input.value}`/gi, "")), container.innerHTML += `${input.value}`.replace(/ /g, "");
+    // console.log(`${input.value}`.replace(/`${input.value}`/gi, "")), container.innerHTML += `${input.value}`.replace(/ /g, "");
+
+    // var reg = new RegExp(input.nodeValue, 'gi')
+
 // })
 
 
- 
+
+// function returnTest(str, ) {
+//     var reg = new RegExp("", "g");
+//     return str.replace(reg, function () {
+//         return ""
+//     })
+// }
+
+
+
+
+/** без пробелов
+ * @description(megafon) ^(8)(92)\d{8}|^\ +(792)\d{8}
+ * @description(mts) ^
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // alert(JSON.stringify(user))
+
+// arr = [{ "name": "sasha", "email": "zoro@qwe.qwe", "sname": "sname", "tel": "+70000000000" }, { "name": "max2", "email": "zoro@qwe.qwe2", "sname": "sname2" }]
+// // console.log(arr[0]);
+// // Object.assign({}, arr)
+// // console.log(arr);
+
+// button1.addEventListener("click", (event) => {
+//     // var clone = arr[0]
+//     // for (let i = 0; i < test.length; i++) {
+//     // console.log(clone);
+//     for (var key in arr) {
+//         var clone = arr[key]
+
+//         // container.innerHTML = container.innerHTML + clone
+//     }
+//     // container.innerHTML +=
+
+//     // }
+// })
