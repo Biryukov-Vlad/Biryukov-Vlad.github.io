3333c37cd8ee49b59da6030f68f8d2033aee2081
Vlad <ksand.1@ya.ru>
2019-09-09 14:14:59 +0400

    localStorege

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const headerNav = document.querySelectorAll(".header__link_js");
 const star = document.querySelectorAll(".header__rating-star_js");
 
+
+
+
 let positionSite = 0;
+// оценки 
+// var arrAssessment = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 var arrStarState = [
     'url("./img/star-white.png") 0 0/contain no-repeat',
     'url("./img/star-gold.png") 0 0/contain no-repeat',
@@ -16,10 +21,53 @@ for (let i = 0; i < headerNav.length; i++) {
     }
 }
 
+// // красим звёзды в записимости от оценки, если её нет то цикл ничего не делает
+// for (let i = 0; i < arrAssessment[positionSite]; i++) {
+//     star[i].style.background = arrStarState[1];
+// }
+// let position;
+// var add = (e) => {
+//     // ищем позицию 
+//     for (let i = 0; i < star.length; i++) {
+//         if (e.target == star[i]) {
+//             position = i;
+//             star[i].style.background = arrStarState[1];
+//             for (let i = 0; i < position; i++) {
+//                 star[i].style.background = arrStarState[1];
+//                 star[i].removeEventListener("click", add)
+//             }
+//             // записать оценку
+//             arrAssessment[positionSite] = position + 1;
+//             console.log(arrAssessment);
+
+//             break;
+//         }
+//     }
+//     // console.log(arrAssessment);
+//     return arrAssessment
+// }
+// // console.log(arrAssessment);
+// if (arrAssessment[positionSite] < 5) {
+//     star[0].addEventListener("click", add)
+//     star[1].addEventListener("click", add)
+//     star[2].addEventListener("click", add)
+//     star[3].addEventListener("click", add)
+//     star[4].addEventListener("click", add)
+// }
+
+// var serialObl = JSON.stringify(arrAssessment)
+// localStorage.setItem("serialObj", serialObl)
+// localStorage.clear("test")
+
+if (!localStorage.getItem("test")) {
+    var clone = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    localStorage.setItem("test", JSON.stringify(clone))
+    console.log(localStorage.getItem("test"));
+
+} 
 
 let newArr = localStorage.getItem("test")
-
-if (newArr !== undefined || newArr !== null) {
+if (localStorage.getItem("test")) {
     newArr = JSON.parse(newArr)
     console.log(newArr);
     for (let i = 0; i < newArr[positionSite]; i++) {
